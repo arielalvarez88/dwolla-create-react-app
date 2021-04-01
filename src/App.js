e527591bc2053e85562d6dd4cd809279bde9cc9f
Ariel Alvarez <arielalvarez88@gmail.com>
2021-04-01 11:28:33 -0400

    putting component in a dialog to show dwolla tab navigation is broken

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import {CreateCustomer} from "./CreateCustomer"
-import './App.css';
+import { CreateCustomer } from "./CreateCustomer";
+import "./App.css";
+import { Dialog } from "primereact/dialog";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
 
 function App() {
   return (
     <div className="App">
-     <CreateCustomer/>
+      <Dialog visible="true">
+        <CreateCustomer />
+      </Dialog>
     </div>
   );
 }
